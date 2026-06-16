271972e4997617f8e798fa52779c52966b6c843e
DavidPannocchi <david.pannocchi@gmail.com>
2026-06-16 15:20:35 +0200

    back to v2

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { initLegacyBehavior, cleanupLegacyBehavior } from './legacyBehavior.js';
-
-const appRoot = document.body;
-
-initLegacyBehavior();
-
-window.addEventListener('beforeunload', () => {
-  cleanupLegacyBehavior();
-});
